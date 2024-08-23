96443f4160a1c040771bac20e6af92df04ad055b
Camila <mendezcamilaitati@gmail.com>
2024-08-23 00:49:10 -0300

    Responsivo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,18 +1,18 @@
 <div>
 
-    <header class="bg-pink-300">
+    <header class="bg-pink-500">
 
             <x-container class="px-4 py-4">
 
-                <div class="flex items-center space-x-8">
+                <div class="flex justify-between items-center space-x-8">
 
-                    <button class="text-3xl">
+                    <button class="text-xl md:text-3xl">
                         <i class="fas fa-bars text-white"></i>
                     </button>
 
                     <h1 class="text-white">
                         <a href="/" class="inline-flex flex-col items-end">
-                            <span class="text-3xl leading-6 font-semibold">
+                            <span class="text-xl md:text-3xl leading-3 md:leading-6 font-semibold">
                                 Elisa Modas
                             </span>
 
@@ -23,23 +23,28 @@
                     </h1>
 
                     {{-- input para la busqueda --}}
-                    <div class="flex-1">
+                    <div class="flex-1 hidden md:block">
                         <x-input class="w-full" placeholder="Buscar producto"/>
                     </div>
 
-                    <div class="space-x-8">
+                    <div class=" flex items-center space-x-4 md:space-x-8">
 
-                        <button class="text-3xl">
+                        <button class="text-xl md:text-3xl">
                             <i class="fas fa-shopping-cart text-white"></i>
                         </button>
 
-                        <button class="text-3xl">
+                        <button class="text-xl md:text-3xl">
                             <i class="fas fa-user text-white"></i>
                         </button>
 
                     </div>
 
                 </div>
+
+                <div class="mt-4 md:hidden">
+                        <x-input class="w-full" placeholder="Buscar producto"/>
+                </div>
+
             </x-container>
 
     </header>
