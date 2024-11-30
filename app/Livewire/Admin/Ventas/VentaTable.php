8fb9cc8b9abb9a84bc3028ecdab108d2ec1821df
Camila <mendezcamilaitati@gmail.com>
2024-11-30 15:59:00 -0300

    Mostrar ventas (tabla sin orden)

---
+++ app/Livewire/Admin/Ventas/VentaTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Livewire\Admin\Ventas;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\Ventas;
+
+class VentaTable extends DataTableComponent
+{
+    protected $model = Ventas::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Nº venta", "id")
+                ->sortable(),
+            Column::make("User id", "user_id")
+                ->sortable(),
+            Column::make("Payment id", "payment_id")
+                ->sortable(),
+            Column::make("Total", "total")
+                ->format(function($value){
+                    return "$".number_format($value, 2);
+                })
+                ->sortable(),
+            Column::make("F. venta", "created_at")
+                ->format(function($value){
+                    return $value->format('d/m/Y');
+                })
+                ->sortable(),
+
+            Column::make("Cantidad", "content")
+                ->format(function($value){
+                    return count($value);
+                })
+                ->sortable(),
+
+            Column::make("Comprobante")
+                ->label(function($row){
+                    return view('admin.ventas.comprobante', ['venta' => $row]);
+                })
+        ];
+    }
+}
---
+++ resources/views/admin/ventas/comprobante.blade.php
@@ -0,0 +1,3 @@
+<button>
+    <img class="h-6" src="/img/iconos/pdf.png " alt="">
+</button>
