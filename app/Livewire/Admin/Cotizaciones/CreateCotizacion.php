5a44c92b4e0c927c269442ea2890845ba361799a
Camila <mendezcamilaitati@gmail.com>
2025-01-20 23:50:12 -0300

    select subcategoria/producto en cotizacion (sin validar nada)

--- app/Livewire/Admin/Cotizaciones/CreateCotizacion.php
+++ app/Livewire/Admin/Cotizaciones/CreateCotizacion.php
@@ -3,9 +3,74 @@
 namespace App\Livewire\Admin\Cotizaciones;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Variant;
+use App\Models\Supplier;
+use App\Models\Subcategory;
 
 class CreateCotizacion extends Component
 {
+    public $subcategory_ids = []; // Agrega esta propiedad para almacenar los IDs de subcategorías seleccionadas
+    public $subcategories = [];
+
+    // Agrega esta propiedad para almacenar las variantes
+    public $variants = [];
+    public $variant_id ;// Agrega esta propiedad para almacenar la variante seleccionada
+
+    public function mount()
+    {
+        //recupero todas las categorias
+        $this->categories = Category::all();
+
+        //recupero todas los proveedores
+        $this->suppliers = Supplier::all();
+
+        $this->subcategories = Subcategory::with('category.family')->get();
+        $this->dispatch('initializeSelect2');
+    }
+
+    public function hydrate()
+    {
+        $this->dispatch('initializeSelect2');
+    }
+
+    //creacion reglas de validacion
+    protected function rules()
+    {
+
+    }
+
+    public function updatedSubcategoryIds($value)
+    {
+        /* Actualizar los IDs de subcategorías cuando cambian
+        $this->subcategory_ids = $value;
+        $this->updateVariants();*/
+        // Asegúrate de que $value sea un array
+        $this->subcategory_ids = is_array($value) ? $value : explode(',', $value);
+        $this->updateVariants();
+    }
+
+    protected function messages()
+    {
+        return [
+            'subcategory_ids.required' => 'Debe seleccionar al menos una subcategoría.',
+            'subcategory_ids.min' => 'Debe seleccionar al menos una subcategoría.',
+        ];
+    }
+
+    public function updateVariants()
+    {
+        if (count($this->subcategory_ids) > 0) {
+            $this->variants = Variant::whereIn('product_id', function($query) {
+                $query->select('id')
+                    ->from('products')
+                    ->whereIn('subcategory_id', $this->subcategory_ids);
+            })->get();
+        } else {
+            $this->variants = [];
+        }
+    }
+
     public function render()
     {
         return view('livewire.admin.cotizaciones.create-cotizacion');
--- resources/views/livewire/admin/cotizaciones/create-cotizacion.blade.php
+++ resources/views/livewire/admin/cotizaciones/create-cotizacion.blade.php
@@ -1,3 +1,89 @@
 <div>
-    holis
+    <x-validation-errors class="mb-4" />
+    <section class="card">
+
+        <header class="border-b px-6 py-2 border-gray-200">
+            <h1>
+                <span class="text-lg font-semibold text-gray-700 dark:text-gray-300">Crear Nueva Cotización</span>
+            </h1>
+        </header>
+
+        <div class="px-6 py-4">
+            <form wire:submit.prevent="createCotizacion">
+
+                <div class="mb-4">
+                    <x-label for="name" value="Nombre" />
+
+                </div>
+
+
+
+                {{-- Seleccionar las subcategorias --}}
+                <div class="mb-4" wire:ignore>
+                    <x-label for="subcategory_id" value="Subcategorías" />
+
+                    <select name="" id="subcategories" class="w-full" multiple="multiple"
+                        wire:model="subcategory_ids">
+                        @foreach ($subcategories as $subcategory)
+                            <option value="{{ $subcategory->id }}">
+                                {{ $subcategory->name }} ({{ $subcategory->category->name }} -
+                                {{ $subcategory->category->family->name }})
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+
+                {{-- Seleccionar las variantes --}}
+                <div class="mb-4">
+                    <x-label for="variant_id" value="Variantes" />
+                    <select name="variant_id" id="variants" class="w-full" wire:model="variant_id">
+                        @foreach ($variants as $variant)
+                            <option value="{{ $variant->id }}">
+                                {{ $variant->sku }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+
+                <div class="flex justify-end mt-4">
+                    <x-button>
+                        Enviar Cotización
+                    </x-button>
+                </div>
+            </form>
+        </div>
+
+    </section>
 </div>
+
+@assets
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+@endassets
+
+
+@script
+    <script>
+        $(document).ready(function() {
+            function initializeSelect2() {
+                $('#subcategories').select2();
+                $('#subcategories').on('change', function(event) {
+                    @this.set('subcategory_ids', $(this).val());
+                });
+            }
+
+            initializeSelect2(); // Inicializar en la primera carga
+
+            Livewire.on('initializeSelect2', function() {
+                setTimeout(function() {
+                    $('#subcategories').select2(
+                    'destroy'); // Destruye select2 antes de volver a aplicarlo
+                    initializeSelect2(); // Vuelve a inicializar select2 después de renderizar
+                }, 100); // Retardo de 100ms para dar tiempo al DOM a estabilizarse
+            });
+        });
+    </script>
+@endscript
