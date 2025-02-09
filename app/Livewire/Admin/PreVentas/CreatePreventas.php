00b6182b054da84f9bea6c66a084ece6e6d7e224
Camila <mendezcamilaitati@gmail.com>
2025-02-09 20:43:45 -0300

    Crear preventa con validaciones

--- app/Livewire/Admin/PreVentas/CreatePreventas.php
+++ app/Livewire/Admin/PreVentas/CreatePreventas.php
@@ -3,11 +3,113 @@
 namespace App\Livewire\Admin\PreVentas;
 
 use Livewire\Component;
+use App\Models\preVenta;
+use App\Models\Variant;
+use Carbon\Carbon;
 
 class CreatePreventas extends Component
 {
+    public $variant_id;
+    public $pool;
+    public $descuento;
+    public $start_date;
+    public $end_date;
+    public $variants = [];
+
+    protected $rules = [
+        'variant_id' => 'required|exists:variants,id',
+        'pool' => 'required|integer|min:1',
+        'descuento' => 'required|integer|min:0|max:100',
+        'start_date' => 'required|date|after_or_equal:today',
+        'end_date' => 'required|date|after:start_date',
+    ];
+
+    public function mount(){
+
+        $this->updateVariants();
+        $this->dispatch('initializeSelect2');
+    }
+
+    public function hydrate()
+    {
+        $this->dispatch('initializeSelect2');
+    }
+
+    public function updateVariants()
+    {
+        $variantData = []; // Array para almacenar los datos formateados
+
+        $this->variants = Variant::where('estado', 'preventa')
+            ->where('stock', 0)
+            ->with('features', 'product') // Cargar características asociadas a las variantes y los productos
+            ->get();
+
+        foreach ($this->variants as $variant) {
+            $productName = $variant->product->name;
+            $variantFeatures = [];
+
+            foreach ($variant->features as $feature) {
+                if ($feature->description) {
+                    $variantFeatures[] = $feature->description;
+                }
+            }
+
+            $featuresString = implode(', ', $variantFeatures);
+
+            $variantData[] = [
+                'id' => $variant->id,
+                'name' => $productName . ($featuresString ? ' (' . $featuresString . ')' : '')
+            ];
+        }
+
+        $this->variants = $variantData;
+    }
+
+    public function submit(){
+
+        $this->validate();
+
+        PreVenta::create([
+            'variant_id' => $this->variant_id,
+            'cantidad' => $this->pool,
+            'descuento' => $this->descuento,
+            'fecha_inicio' => Carbon::parse($this->start_date),
+            'fecha_fin' => Carbon::parse($this->end_date),
+            'estado' => 'preventa',
+        ]);
+
+
+        session()->flash('swal', [
+            'icon' => 'success',
+            'title' => 'Bien hecho!',
+            'text' => 'La preventa ha sido creada correctamente',
+        ]);
+
+        return redirect()->route('admin.pre-ventas.index');
+    }
+
+
+
+    public function boot()
+    {
+        $this->withValidator(function ($validator) {
+            // Verificar si hay errores de validación
+            if ($validator->fails()) {
+                // Disparar una alerta con los detalles del error
+                $this->dispatch('swal', [
+                    'icon' => 'error',
+                    'title' => 'Error!',
+                    'text' => 'El formulario contiene errores.',
+                ]);
+            }
+        });
+    }
+
     public function render()
     {
-        return view('livewire.admin.pre-ventas.create-preventas');
+        $variants = Variant::where('estado', 'preventa')
+            ->where('stock', 0)
+            ->get();
+        return view('livewire.admin.pre-ventas.create-preventas', compact('variants'));
     }
 }
--- resources/views/livewire/admin/pre-ventas/create-preventas.blade.php
+++ resources/views/livewire/admin/pre-ventas/create-preventas.blade.php
@@ -1,3 +1,79 @@
-<div>
-    agora
+<div class="card">
+    <x-validation-errors class="mb-4" />
+    <form wire:submit.prevent="submit">
+        <div class="mb-4">
+            <x-label for="variant_id" value="Variante" />
+            <select wire:model="variant_id" id="variant_id" class="block mt-1 w-full">
+                <option value="">Seleccione una variante</option>
+                @foreach ($variants as $variant)
+                    <option value="{{ $variant['id'] }}">{{ $variant['name'] }}</option>
+                @endforeach
+            </select>
+            <x-input-error for="variant_id" class="mt-2" />
+        </div>
+
+        <div class="mb-4">
+            <x-label for="pool" value="Cantidad de unidades disponibles" />
+            <x-input wire:model="pool" id="pool" type="number" class="block mt-1 w-full" />
+            <x-input-error for="pool" class="mt-2" />
+        </div>
+
+        <div class="mb-4">
+            <x-label for="descuento" value="Descuento (%)" />
+            <x-input wire:model="descuento" id="descuento" type="number" class="block mt-1 w-full" />
+            <x-input-error for="descuento" class="mt-2" />
+        </div>
+
+        <div class="mb-4">
+            <x-label for="start_date" value="Fecha de inicio" />
+            <x-input wire:model="start_date" id="start_date" type="date" class="block mt-1 w-full" />
+            <x-input-error for="start_date" class="mt-2" />
+        </div>
+
+        <div class="mb-4">
+            <x-label for="end_date" value="Fecha de fin" />
+            <x-input wire:model="end_date" id="end_date" type="date" class="block mt-1 w-full" />
+            <x-input-error for="end_date" class="mt-2" />
+        </div>
+
+        <div class="flex justify-between">
+            {{-- Botón para volver atrás --}}
+            <button onclick="window.history.back()"
+                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                Volver Atrás
+            </button>
+
+            <x-button>
+                Crear Preventa
+            </x-button>
+        </div>
+    </form>
 </div>
+
+@assets
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+@endassets
+
+
+<script>
+    $(document).ready(function() {
+        function initializeSelect2() {
+            $('#variant_id').select2();
+            $('#variant_id').on('change', function(event) {
+                @this.set('variant_id', $(this).val());
+            });
+        }
+
+        initializeSelect2(); // Inicializar en la primera carga
+
+        Livewire.on('initializeSelect2', function() {
+            setTimeout(function() {
+                $('#variant_id').select2('destroy'); // Destruye select2 antes de volver a aplicarlo
+                initializeSelect2(); // Vuelve a inicializar select2 después de renderizar
+            }, 100); // Retardo de 100ms para dar tiempo al DOM a estabilizarse
+        });
+    });
+</script>
+
